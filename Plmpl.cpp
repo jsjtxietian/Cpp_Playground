601e558cd6f33785771ee72083cdce1a35e4c437
jsjtxietian <jsjtxietian@outlook.com>
2023-02-13 15:10:15 +0800

    plmpl and bridge performance

---
+++ Plmpl.cpp
@@ -0,0 +1,90 @@
+// Build Bridges to Remove Physical Dependencies
+
+//---- <Person.h> ---------------------------------------------------------------------------------
+
+#include <memory>
+
+class Person
+{
+public:
+    // ...
+    Person();
+    ~Person();
+
+    Person(Person const &other);
+    Person &operator=(Person const &other);
+
+    Person(Person &&other);
+    Person &operator=(Person &&other);
+
+    int year_of_birth() const;
+    // ... Many more access functions
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> const pimpl_;
+};
+
+//---- <Person.cpp> -------------------------------------------------------------------------------
+
+// #include <Person.h>
+#include <string>
+
+struct Person::Impl
+{
+    std::string forename;
+    std::string surname;
+    std::string address;
+    std::string city;
+    std::string country;
+    std::string zip;
+    int year_of_birth;
+    // ... Potentially many more data members
+};
+
+Person::Person()
+    : pimpl_{std::make_unique<Impl>()}
+{
+}
+
+Person::~Person() = default;
+
+Person::Person(Person const &other)
+    : pimpl_{std::make_unique<Impl>(*other.pimpl_)}
+{
+}
+
+Person &Person::operator=(Person const &other)
+{
+    *pimpl_ = *other.pimpl_;
+    return *this;
+}
+
+Person::Person(Person &&other)
+    : pimpl_{std::make_unique<Impl>(std::move(*other.pimpl_))}
+{
+}
+
+Person &Person::operator=(Person &&other)
+{
+    *pimpl_ = std::move(*other.pimpl_);
+    return *this;
+}
+
+int Person::year_of_birth() const
+{
+    return pimpl_->year_of_birth;
+}
+
+// ... Many more Person member functions
+
+//---- <Main.cpp> ---------------------------------------------------------------------------------
+
+// #include <Person.h>
+
+int main()
+{
+    Person p1{};
+
+    return EXIT_SUCCESS;
+}
